7af37328d4b77a8032de64d9a66a79d64a99cd76
Agrim <agrimgupta31@gmail.com>
2025-09-14 15:46:42 +0530

    import error corrected

--- iris-rf.py
+++ iris-rf.py
@@ -2,7 +2,7 @@ import mlflow
 import mlflow.sklearn
 from sklearn.datasets import load_iris
 from sklearn.model_selection import train_test_split
-from sklearn.tree import RandomForestClassifier
+from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score,confusion_matrix, classification_report
 import matplotlib.pyplot as plt
 import seaborn as sns
